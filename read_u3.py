7a4286a58fc4f7f26048af5780b12a1e1ce7c968
ryanm161 <67024920+ryanm161@users.noreply.github.com>
2022-10-14 17:53:35 -0700

    Add files via upload

--- read_u3.py
+++ read_u3.py
@@ -2,25 +2,32 @@ import u3
 import u6
 
 serial = [320091553,320096837,320091728,320096852] #Serial numbers of four U3 labjacks
+id = ["AIN","EIO"]
 
-for j in range(4):
+def read(ljstart,ljrange,index,shift):
+    for i in range(ljrange):
+        v = round(d.getAIN(i+ljstart),3)
+        print("   " + id[index] + " " + str(i+shift) + ": " + str(v) + " V")
+
+for j in range(2):
     d = u3.U3(autoOpen = False)
     d.open(serial = serial[j]) #Connect multiple U3 labjacks
     d.configIO(FIOAnalog=255, EIOAnalog=255) #Set all pins to analog
     d.getCalibrationData()
+    print("Labjack " + str(j) + " (U3) (" + str(serial[j]) + "):")
+    read(0,4,0,0)
+    read(8,8,1,0)
 
-    print("Labjack " + str(j) + " (" + str(serial[j]) + "):")
-    for i in range(16):
-        v = round(d.getAIN(i),3)
-        if -0.5  < v < 0.5:
-            v = 0 #Ignore small inaccuracies in measurement for voltages near zero
-        print("   Channel " + str(i) + " voltage: " + str(v))
+for j in range(2):
+    d = u3.U3(autoOpen = False)
+    d.open(serial = serial[j+2]) #Connect multiple U3 labjacks
+    d.configIO(FIOAnalog=255, EIOAnalog=255) #Set all pins to analog
+    d.getCalibrationData()
+    print("Labjack " + str(j+2) + " (U3) (" + str(serial[j+2]) + "):")
+    read(0,4,0,2)
 
 d = u6.U6()
 d.getCalibrationData()
 print("Labjack 4 (U6):")
-for i in range(16):
-    v = d.getAIN(i)
-    if -0.5  < v < 0.5:
-        v = 0 #Ignore small inaccuracies in measurement for voltages around zero
-    print("   Channel " + str(i) + " voltage: " + str(v))
+read(0,4,0,0)
+read(10,10,1,10)
