a6f1738ff2a216dabfd77eb23b8fe8b3f0d36b64
ryanm161 <67024920+ryanm161@users.noreply.github.com>
2022-10-14 18:57:07 -0700

    Add files via upload

--- read_u3.py
+++ read_u3.py
@@ -4,10 +4,10 @@ import u6
 serial = [320091553,320096837,320091728,320096852] #Serial numbers of four U3 labjacks
 id = ["AIN","EIO"]
 
-def read(ljstart,ljrange,index,shift):
+def read(ljstart,ljrange,index):
     for i in range(ljrange):
         v = round(d.getAIN(i+ljstart),3)
-        print("   " + id[index] + " " + str(i+shift) + ": " + str(v) + " V")
+        print("   " + id[index] + " " + str(i+ljstart) + ": " + str(v) + " V")
 
 for j in range(2):
     d = u3.U3(autoOpen = False)
@@ -15,8 +15,8 @@ for j in range(2):
     d.configIO(FIOAnalog=255, EIOAnalog=255) #Set all pins to analog
     d.getCalibrationData()
     print("Labjack " + str(j) + " (U3) (" + str(serial[j]) + "):")
-    read(0,4,0,0)
-    read(8,8,1,0)
+    read(0,4,0)
+    read(8,8,1)
 
 for j in range(2):
     d = u3.U3(autoOpen = False)
@@ -24,10 +24,10 @@ for j in range(2):
     d.configIO(FIOAnalog=255, EIOAnalog=255) #Set all pins to analog
     d.getCalibrationData()
     print("Labjack " + str(j+2) + " (U3) (" + str(serial[j+2]) + "):")
-    read(0,4,0,2)
+    read(0,4,0)
 
 d = u6.U6()
 d.getCalibrationData()
 print("Labjack 4 (U6):")
-read(0,4,0,0)
-read(10,10,1,10)
+read(0,4,0)
+read(10,10,1)
